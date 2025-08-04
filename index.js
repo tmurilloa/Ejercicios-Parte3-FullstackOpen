fd2d5fbb88894e80e626b5929f4cf1c0854dadbe
AMD <tmurilloa@unal.edu.co>
2025-08-04 16:56:08 -0500

    Ejercicio 3.9 hecho

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('body', (req, res) => {
     return JSON.stringify(req.body)
@@ -107,7 +109,7 @@ app.post('/api/persons', (request, response)=>{
 
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
