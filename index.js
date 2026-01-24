fdc439415e42913a17f7f90c3db42b385469980e
AMD <tmurilloa@unal.edu.co>
2026-01-23 21:36:06 -0500

    Hecho Ejercicios 3.13- 3.14

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+const Person = require('./models/person')
 
 app.use(express.json())
 app.use(cors())
@@ -14,56 +16,33 @@ morgan.token('body', (req, res) => {
 
 app.use(morgan(':method :url :status :response-time ms :body'))
 
-let datos = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 
 /* Funciones GET */ 
 app.get('/info', (request, response) => {
     const time = new Date()
-
-    response.send(`
-        <p>Phonebook has info for ${datos.length} people</p>
+    Person.countDocuments({}).then(result => {
+        response.send(`
+        <p>Phonebook has info for ${result} people</p>
         <p>${time}</p>
         `)
+    })
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(datos)
+    Person.find({}).then(result => {
+        response.json(result)
+    }) 
 })
 
 app.get('/api/persons/:id', (request, response) =>{
-    const id = Number(request.params.id)
-    const person = datos.find(person => person.id === id)
-
-    if(person){
+    Person.findById(request.params.id).then(person =>{
+        if(person){
         response.json(person)
-    }
-    else{
-        response.status(404).end()
-    }
-
-
+        }
+        else{
+            response.status(404).end()
+        }
+    })
 })
 
 /* Funciones DELETE */
@@ -84,32 +63,20 @@ const generateID = () => {
 app.post('/api/persons', (request, response)=>{
     const body = request.body
     
-
-    if (!body.name && !body.number){
-        return response.status(400).json({
-            error: 'name or number missing'
-        })
+    if (!body.name || !body.number){
+        return response.status(400).json({error: 'name or number missing'})
     }
 
-    const rep_name = datos.find(person => person.name === body.name)
+    const person = new Person({
+        name: body.name,
+        number: body.number
+    })
 
-    if (rep_name){
-        return response.status(400).json({
-            error: 'name already in data'
-        })
-    }
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
 
-    const person = {
-        "id": generateID(),
-        "name": body.name,
-        "number": body.number
-    }
-
-    datos = datos.concat(person)
-    response.json(person)
 })
-
-
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
